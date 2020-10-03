c5ad762b1380f614c614bc67b145321a2e85ccb2
tonny <tonny_1983@hotmail.com>
2020-10-03 17:10:23 +0800

    add page controller

--- src/main/java/cc/tonny/multisecurityconfig/adapter/inbound/PageController.java
+++ src/main/java/cc/tonny/multisecurityconfig/adapter/inbound/PageController.java
@@ -1,4 +1,17 @@
 package cc.tonny.multisecurityconfig.adapter.inbound;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PageController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping(value = {"/", "/index"})
+    public String index() {
+        return "index";
+    }
 }
